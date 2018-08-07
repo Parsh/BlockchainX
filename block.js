860029dab45115c4db6ad1adfba8f77538b6896c
parsh24 <parsh.cosmos11@gmail.com>
2018-08-08 00:26:39 +0530

    Basic Layout: Block Class

---
+++ block.js
@@ -0,0 +1,20 @@
+class Block{
+    constructor(timestamp, prevHash, hash, data){
+        this.timestamp = timestamp;
+        this.prevHash = prevHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString(){
+        //becuase the hashs are going to be quite long, we used the subString function
+        //so as to log a concise, not to mention readable, output.
+        return `Block --
+        Timestamp             : ${this.timestamp}
+        Previous Block's Hash : ${this.prevHash.substring(0, 10)}
+        Current Block's Hash  : ${this.hash.substring(0, 10)}
+        Block's Data          : ${this.data} `;
+    }
+}
+
+module.exports = Block;
