19a0a618b2d4c5bcad3cd104c71fcb44c291eac7
parsh24 <parsh.cosmos11@gmail.com>
2018-08-08 01:10:34 +0530

    SHA256 incorporated

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
     constructor(timestamp, prevHash, hash, data){
         this.timestamp = timestamp;
@@ -24,9 +26,13 @@ class Block{
     static mineBlock(prevBlock, data){
         const timestamp = Date.now();
         const prevHash = prevBlock.hash;
-        const hash = 'todo-hash';
+        const hash = Block.createHash(timestamp, prevHash, data);
 
         return new this(timestamp, prevHash, hash, data);
+    }
+
+    static createHash(timestamp, prevHash, data){
+       return SHA256(`${timestamp}${prevHash}${data}`).toString();
     }
 
 }
