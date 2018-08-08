f194834913317a248384acf92a73f23e9d978dea
parsh24 <parsh.cosmos11@gmail.com>
2018-08-08 11:21:30 +0530

    Validating a chain

--- block.js
+++ block.js
@@ -19,11 +19,14 @@ class Block{
     }
 
     static genesis(){
-        //genesis block, typically hardcoded with dummy values 
+        //create and returns a genesis block (typically hardcoded with dummy values) 
+
         return new this('Genesis Time', '-------', 'gen321nm98m2aek', []);
     }
 
     static mineBlock(prevBlock, data){
+        //mines(creates) a block 
+
         const timestamp = Date.now();
         const prevHash = prevBlock.hash;
         const hash = Block.createHash(timestamp, prevHash, data);
@@ -32,9 +35,22 @@ class Block{
     }
 
     static createHash(timestamp, prevHash, data){
+        //generates and returns a SHA256 digest
+
        return SHA256(`${timestamp}${prevHash}${data}`).toString();
     }
 
+    static blockHash(block){
+        //calculates the hash for a supplied block
+
+        // const timestamp = block.timestamp;
+        // const prevHash = block.prevHash;
+        // const data = block.data; 
+        //(Following is an ES6 short hand for the above assignments)
+        const {timestamp, prevHash, data} = block; //corresponding attribute values are assigned
+        
+        return Block.createHash(timestamp, prevHash, data);
+    }
 }
 
 module.exports = Block;
--- blockchain.js
+++ blockchain.js
@@ -6,11 +6,30 @@ class Blockchain{
     }
 
     addBlock(data){
-        const lastBlock = this.chain[this.chain.length-1];
-        const block = Block.mineBlock(lastBlock, data);
+        //add a block, with the provided data, to the blockchain
+
+        const prevBlock = this.chain[this.chain.length-1];
+        const block = Block.mineBlock(prevBlock, data);
         this.chain.push(block);
 
         return block;
+    }
+
+    isChainValid(chainx){
+        //checks whether the supplied chain is valid
+
+        if (JSON.stringify(chainx[0]) !== JSON.stringify(Block.genesis())) return false;
+
+        for (let blockNum =1; blockNum < chian.length; blockNum++){
+            const currentBlock = chainx[blockNum];
+            const prevBlock = chainx[blockNum - 1];
+
+            if (currentBlock.prevHash !== prevBlock.hash ||
+                block.hash !== Block.blockHash(block)){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
