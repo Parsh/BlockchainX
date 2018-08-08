20bdbe15c075fd7db126c4918cdfcd2a548040c8
parsh24 <parsh.cosmos11@gmail.com>
2018-08-08 14:44:30 +0530

    Replacing the Blockchain

--- blockchain.js
+++ blockchain.js
@@ -31,6 +31,19 @@ class Blockchain{
         }
         return true;
     }
+
+    replaceChain(newChain){
+        //replaces the current blockchain with a new valid and longer blockchain
+
+        if (newChain.length <= this.chain.length){
+            console.log("Supplied chain isn't longer(less PoW) than the current chain");
+        } else if (!this.isValidChain(newChain)){
+            console.log("The recieved chain isn't valid");
+        } else {
+            console.log("Replacing the current blockchain with the supplied one #Long&Valid");
+            this.chain = newChain
+        }
+    }
 }
 
 module.exports = Blockchain;
