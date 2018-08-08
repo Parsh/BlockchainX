deff3fe52a8311330b8f4c1369bab3992d344fca
parsh24 <parsh.cosmos11@gmail.com>
2018-08-08 19:29:40 +0530

    Web API Layout

---
+++ app/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain/blockchain');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome To BlockchainX<h1>")
+});
+
+app.get('/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+    console.log(req)
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New blocks added ${block.toString()}`)
+    res.redirect('/blocks');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server up and listening at ${PORT}`)
+});
