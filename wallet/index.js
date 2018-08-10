0096e1df71073d81226ca9e9af114d1a4adf9d18
parsh24 <parsh.cosmos11@gmail.com>
2018-08-10 10:53:38 +0530

    Integrated transaction creation mechanism into Wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util');
+const Transaction = require('./transaction');
 const { INITIAL_BALANCE } = require('../config');
 
 class Wallet {
@@ -19,6 +20,29 @@ class Wallet {
 
         return this.keyPair.sign(dataHash);
     }
+
+    createTransaction(recipient, amount, transactionPool){
+        // creates or updates a transaction and sends it to the transactionPool
+
+        if(amount > this.balance){
+            cosnole.log(`Transaction not created, Amount: ${amount} exceeds current balance: ${this.balance}`);
+            return
+        }
+
+        let transaction = transactionPool.existingTransaction(this.publicKey); 
+        //checking whether there already exist a transaction in the pool from this wallet
+
+        if(transaction){
+            transaction.update(this, recipient, amount)
+        } else {
+            transaction = Transaction.newTransaction(this, recipient, amount);
+        }
+
+        transactionPool.updateOrAddTransaction(transaction);
+        return transaction;
+
+    }
+
 }
 
 module.exports = Wallet;
