852241ca841e272bfdbf277ba6c6392927f338bd
parsh24 <parsh.cosmos11@gmail.com>
2018-08-10 15:07:02 +0530

    Pooling Valid Transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require('./transaction');
+
 class TransactionPool{
     constructor(){
         this.transactions = [];
@@ -21,6 +23,26 @@ class TransactionPool{
 
     existingTransaction(address){
         return this.transactions.find(t => t.input.address === address);
+    }
+
+    validTransaction(){
+        return this.transactions.filter(transaction => {
+            const outputTotal = transaction.outputs.reduce((total, output) =>{
+                total = total + output.amount;
+            }, 0);
+
+            if(transaction.input.amount !== outputTotal){
+                console.log(`Invalid transaction from ${transaction.input.address}.`);
+                return;
+            }
+
+            if(!Transaction.verifyTransaction(transaction)){
+                console.log(`Invalid signature from ${transaction.input.address}`);
+                return;
+            }
+
+            return transaction;
+        });
     }
 
 
