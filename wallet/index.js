0b22eac03e3dac40094f38803b784c7ddcfd66df
parsh24 <parsh.cosmos11@gmail.com>
2018-08-10 21:40:49 +0530

    Wallet Balance Calculation Mechanism

--- wallet/index.js
+++ wallet/index.js
@@ -43,6 +43,48 @@ class Wallet {
 
     }
 
+    calculateBalance(blockchain){
+        //calculates the current balance of the wallet
+
+        let balance = this.balance;
+        let transactions = [];
+
+         //collecting all the transactions from the blockchain into a single array
+        blockchain.chain.forEach(block => block.data.forEach(transaction => {
+            transactions.push(transaction); 
+        }));
+
+        //from the array of all transactions, capture only those transactions that were initiated by this wallet
+        const walletInputs = transactions.filter(transactions => transactions.input.address === this.publicKey);
+        
+        let startTime = 0;
+
+        //find the most recent transaction by this wallet using the timestamp
+       if(walletInputs.length > 0){ //JS reduce fxn cann't reduce an array that's empty, hence we check
+            const recentInputTransaction = walletInputs.reduce(
+                (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current
+            );
+
+            //assign the current balance to be equal to the recent transaction's change 
+            balance = recentInputTransaction.outputs.find(output => output.address === this.publicKey).amount;
+        
+            startTime = recentInputTransaction.input.timestamp; //time of the most recent transaction from the wallet
+        }
+
+        //for all the transactions that occured after the recent transaction from this wallet, check whether they
+        //contain an output with an address equal to this wallet's publicKey(address), if so, then add the output's
+        //amount to the wallet's balance
+        transactions.forEach(transaction =>  {
+            if(transaction.input.timestamp > startTime){
+                transaction.outputs.find(output => {
+                    if(output.address === this.publicKey){
+                        balance += output.amount;
+                    }
+                })
+            }
+        });    
+    }
+
     static blockchainWallet(){
         //generates an instance of the special blockchain wallet
 
